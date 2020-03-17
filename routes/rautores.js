dd1c1c763af2f65846c9843c079fd000a76d2caa
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-03-17 10:58:26 +0100

    SDI-session7-Ejercicio-complementario3 No se filtra bien

--- routes/rautores.js
+++ routes/rautores.js
@@ -1,18 +1,29 @@
 module.exports = function (app, swig) {
+    var roles = [{
+        "rol": "cantante"
+    }, {
+        "rol": "bateria"
+    }, {
+        "rol": "guitarrista"
+    }, {
+        "rol": "bajista"
+    }, {
+        "rol": "teclista"
+    }];
+    var autores = [{
+        "nombre": "pedro",
+        "grupo": "AC/DC",
+        "rol": "Bateria"
+    }, {
+        "nombre": "Alex",
+        "grupo": "AC/DC",
+        "rol": "Bajo"
+    }, {
+        "nombre": "Fran",
+        "grupo": "AC/DC",
+        "rol": "Cantante"
+    }];
     app.get("/autores", function (req, res) {
-        var autores = [{
-            "nombre": "pedro",
-            "grupo": "AC/DC",
-            "rol": "Bateria"
-        }, {
-            "nombre": "Alex",
-            "grupo": "AC/DC",
-            "rol": "Bajo"
-        }, {
-            "nombre": "Fran",
-            "grupo": "AC/DC",
-            "rol": "Cantante"
-        }];
         var respuesta = swig.renderFile('views/autores.html', {
             vendedor: 'Tienda de canciones',
             autores: autores
@@ -20,23 +31,20 @@ module.exports = function (app, swig) {
         res.send(respuesta);
     });
     app.get('/autores/agregar', function (req, res) {
-        var roles = [{
-            "rol": "cantante"
-        }, {
-            "rol": "bateria"
-        }, {
-            "rol": "guitarrista"
-        }, {
-            "rol": "bajista"
-        }, {
-            "rol": "teclista"
-        }];
         let respuesta = swig.renderFile('views/autores-agregar.html', {
             vendedor: 'Tienda de canciones',
             roles: roles
         });
         res.send(respuesta);
     })
+    app.get('/autores/filtrar/:id', function (req, res) {
+        let respuestaAutores= autores.filter(a => a.rol == req.params.id);
+        let respuesta = swig.renderFile('views/autores.html', {
+            vendedor: 'Tienda de canciones',
+            autores: respuestaAutores
+        });
+        res.send(respuesta);
+    });
     app.post("/autores", function (req, res) {
         res.send("Autor agregado:" + req.body.nombre + "<br>"
             + " Grupo :" + req.body.grupo + "<br>"
