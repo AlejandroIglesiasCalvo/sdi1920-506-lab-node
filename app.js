4ec6ed6faba5c7beb27c390788ef3403fb047cf5
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-03-22 15:39:54 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 app.use(express.static('public'));
 
@@ -15,8 +17,8 @@ app.set('port', 8081);
 app.set('db',' mongodb://admin:admin@tiendamusica-shard-00-00-en3ox.mongodb.net:27017,tiendamusica-shard-00-01-en3ox.mongodb.net:27017,tiendamusica-shard-00-02-en3ox.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
 //Rutas/controladores por lógica
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig,mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
 require("./routes/rautores.js")(app, swig);
 // lanzar el servidor
 app.listen(app.get('port'), function() {
